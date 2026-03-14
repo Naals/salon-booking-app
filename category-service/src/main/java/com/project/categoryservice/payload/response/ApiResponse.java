3e52a1a35eb914600e446ad9dc49869bebb7248c
Aslan <tynyshtykaslan06@gmail.com>
2026-03-14 12:06:33 +0500

    class for responses

---
+++ category-service/src/main/java/com/project/categoryservice/payload/response/ApiResponse.java
@@ -0,0 +1,11 @@
+package com.project.categoryservice.payload.response;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse {
+    private String message;
+}
