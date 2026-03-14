51be3c7c511e5e94060c308bea6439fa6a7a0fb4
Aslan <tynyshtykaslan06@gmail.com>
2026-03-14 12:05:56 +0500

    Category Repository init

---
+++ category-service/src/main/java/com/project/categoryservice/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.project.categoryservice.repository;
+
+import com.project.categoryservice.modal.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Set<Category> findBySalonId(Long salonId);
+}
