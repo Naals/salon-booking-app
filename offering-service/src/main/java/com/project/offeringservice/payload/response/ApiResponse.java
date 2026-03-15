6fd0aa97fc8a7f8c9a19029cf7786be280533a32
Aslan <tynyshtykaslan06@gmail.com>
2026-03-15 14:48:34 +0500

    Api Response class

---
+++ offering-service/src/main/java/com/project/offeringservice/payload/response/ApiResponse.java
@@ -0,0 +1,11 @@
+package com.project.offeringservice.payload.response;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse {
+    private String message;
+}
