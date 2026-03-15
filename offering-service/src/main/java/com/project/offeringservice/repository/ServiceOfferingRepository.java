cd27e3aa662bb69632ceed56a6720bf0649618aa
Aslan <tynyshtykaslan06@gmail.com>
2026-03-15 14:49:48 +0500

    ServiceOffering repository

---
+++ offering-service/src/main/java/com/project/offeringservice/repository/ServiceOfferingRepository.java
@@ -0,0 +1,11 @@
+package com.project.offeringservice.repository;
+
+import com.project.offeringservice.modal.ServiceOffering;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+
+public interface ServiceOfferingRepository extends JpaRepository<ServiceOffering, Long> {
+
+    Set<ServiceOffering> findBySalonId(Long salonId);
+}
