c150439cceaf68e82c77152d643164c7d0c413da
Aslan <tynyshtykaslan06@gmail.com>
2026-03-18 14:50:14 +0500

    Modals

---
+++ booking-service/src/main/java/com/project/bookingservice/modal/SalonReport.java
@@ -0,0 +1,14 @@
+package com.project.bookingservice.modal;
+
+import lombok.Data;
+
+@Data
+public class SalonReport {
+    private Long salonId;
+    private String salonName;
+    private Double totalEarnings;
+    private Integer totalBookings;
+    private Integer cancelledBookings;
+    private Double totalRefund;
+
+}
