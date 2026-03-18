d08de4427d2f2689e3b14cde4722d7bbae047f57
Aslan <tynyshtykaslan06@gmail.com>
2026-03-18 14:24:47 +0500

    ServiceOffering Controller init

---
+++ offering-service/src/main/java/com/project/offeringservice/controller/ServiceOfferingController.java
@@ -0,0 +1,43 @@
+package com.project.offeringservice.controller;
+
+import com.project.offeringservice.modal.ServiceOffering;
+import com.project.offeringservice.services.ServiceOfferingService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/service-offering")
+@RequiredArgsConstructor
+public class ServiceOfferingController {
+
+    private final ServiceOfferingService serviceOfferingService;
+
+    @GetMapping("/salon/{salonId}")
+    public ResponseEntity<Set<ServiceOffering>> getServicesBySalonId(
+            @PathVariable Long salonId,
+            @RequestParam(required = false) Long categoryId
+    ) {
+        return ResponseEntity.ok(serviceOfferingService.getAllServicesBySalonId(salonId, categoryId));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ServiceOffering> getServicesById(
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(serviceOfferingService.getServiceById(id));
+    }
+
+    @GetMapping("/list/{ids}")
+    public ResponseEntity<Set<ServiceOffering>> getServicesByIds(
+            @PathVariable Set<Long> ids
+    ) {
+        return ResponseEntity.ok(serviceOfferingService.getServiceByIds(ids));
+    }
+
+
+
+}
