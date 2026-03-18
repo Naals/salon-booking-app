f56652fef1a3418b35c52b470a823dd92fef8695
Aslan <tynyshtykaslan06@gmail.com>
2026-03-18 14:55:28 +0500

    Booking Repository

---
+++ booking-service/src/main/java/com/project/bookingservice/repository/BookingRepository.java
@@ -0,0 +1,11 @@
+package com.project.bookingservice.repository;
+
+import com.project.bookingservice.modal.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByCustomerId(Long customerId);
+    List<Booking> findBookingSalonId(Long salonId);
+}
