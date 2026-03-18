f477626c3c5d6750baa6521642e6309043eeeb52
Aslan <tynyshtykaslan06@gmail.com>
2026-03-18 14:49:22 +0500

    side dtos

---
+++ booking-service/src/main/java/com/project/bookingservice/payload/dto/SalonDto.java
@@ -0,0 +1,32 @@
+package com.project.bookingservice.payload.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class SalonDto {
+
+    private Long id;
+
+    private String name;
+
+    private List<String> images;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private String city;
+
+    private Long ownerId;
+
+    private UserDto owner;
+
+    private LocalDateTime openTime;
+
+    private LocalDateTime closeTime;
+}
---
+++ booking-service/src/main/java/com/project/bookingservice/payload/dto/UserDto.java
@@ -0,0 +1,24 @@
+package com.project.bookingservice.payload.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UserDto {
+
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+    private String phone;
+
+    private String role;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
