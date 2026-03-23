a3d00cd01d68aea38d9ff907605ad1ac5925a6b5
Aslan <tynyshtykaslan06@gmail.com>
2026-03-23 14:49:23 +0500

    new method

--- booking-service/src/main/java/com/project/bookingservice/repository/BookingRepository.java
+++ booking-service/src/main/java/com/project/bookingservice/repository/BookingRepository.java
@@ -2,10 +2,18 @@ package com.project.bookingservice.repository;
 
 import com.project.bookingservice.modal.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByCustomerId(Long customerId);
-    List<Booking> findBookingSalonId(Long salonId);
+    List<Booking> findBySalonId(Long salonId);
+    List<Booking> findBySalonIdAndStartTimeBetween(
+            Long salonId,
+            LocalDateTime start,
+            LocalDateTime end
+    );
 }
