d9ffd59f044a00a8260e394e83a04c3e4b7e9644
Aslan <tynyshtykaslan06@gmail.com>
2026-03-24 22:17:42 +0500

    other dbs

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,1 +1,6 @@
 CREATE DATABASE sb_booking_db;
+CREATE DATABASE sb_payment_db;
+CREATE DATABASE sb_category_db;
+CREATE DATABASE sb_offering_service_db;
+CREATE DATABASE sb_salon_db;
+CREATE DATABASE sb_user_db;
