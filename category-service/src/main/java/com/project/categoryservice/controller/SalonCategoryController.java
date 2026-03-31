52cb93fc2c30afa6e064f9c514c4148c244540b7
Aslan <tynyshtykaslan06@gmail.com>
2026-03-31 23:18:19 +0500

    feat: authentication in category service

--- category-service/src/main/java/com/project/categoryservice/controller/SalonCategoryController.java
+++ category-service/src/main/java/com/project/categoryservice/controller/SalonCategoryController.java
@@ -4,9 +4,11 @@ import com.project.categoryservice.payload.dto.SalonDto;
 import com.project.categoryservice.modal.Category;
 import com.project.categoryservice.payload.response.ApiResponse;
 import com.project.categoryservice.service.CategoryService;
+import com.project.categoryservice.service.client.SalonFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -14,23 +16,38 @@ import org.springframework.web.bind.annotation.*;
 public class SalonCategoryController {
 
     private final CategoryService categoryService;
+    private final SalonFeignClient salonFeignClient;
 
     @PostMapping()
-    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
-        SalonDto salonDto = new SalonDto();
-        salonDto.setId(1L);
+    public ResponseEntity<Category> createCategory(
+            @RequestBody Category category,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        SalonDto salonDto = salonFeignClient.getSalonByOwnerId(jwt).getBody();
 
         Category saveCategory = categoryService.saveCategory(category, salonDto);
         return ResponseEntity.ok(saveCategory);
     }
 
+    @GetMapping("salon/{salonId}/category/{id}")
+    public ResponseEntity<Category> getCategoriesByIdAndSalon(
+            @PathVariable Long id,
+            @PathVariable Long salonId
+    ) {
+        Category categories = categoryService.getCategoryByIdAndSalonId(id, salonId);
+        return ResponseEntity.ok(categories);
+    }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id) throws Exception {
-        SalonDto salonDto = new SalonDto();
-        salonDto.setId(1L);
+    public ResponseEntity<ApiResponse> deleteCategory(
+            @PathVariable Long id,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
 
+        SalonDto salonDto = salonFeignClient.getSalonByOwnerId(jwt).getBody();
+
+        assert salonDto != null;
         categoryService.deleteCategoryById(id, salonDto.getId());
-
 
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setMessage("Category deleted successfully");
