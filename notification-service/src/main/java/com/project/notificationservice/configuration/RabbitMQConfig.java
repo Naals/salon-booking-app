f90e2e73d4e9daa82feaee3be7942f8bc1069b60
Aslan <tynyshtykaslan06@gmail.com>
2026-04-02 23:45:08 +0500

    feat: rabbitmq configuration

---
+++ notification-service/src/main/java/com/project/notificationservice/configuration/RabbitMQConfig.java
@@ -0,0 +1,98 @@
+package com.project.notificationservice.configuration;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+
+    public static final String PAYMENT_QUEUE = "payment-queue";
+    public static final String NOTIFICATION_QUEUE = "notification-queue";
+    public static final String BOOKING_QUEUE = "booking-queue";
+
+
+    public static final String PAYMENT_EXCHANGE = "payment-exchange";
+    public static final String NOTIFICATION_EXCHANGE = "notification-exchange";
+    public static final String BOOKING_EXCHANGE = "booking-exchange";
+
+
+    public static final String PAYMENT_ROUTING_KEY = "payment.routing.key";
+    public static final String NOTIFICATION_ROUTING_KEY = "notification.routing.key";
+    public static final String BOOKING_ROUTING_KEY = "booking.routing.key";
+
+    // --- Очереди ---
+    @Bean
+    public Queue paymentQueue() {
+        return new Queue(PAYMENT_QUEUE, true);
+    }
+
+    @Bean
+    public Queue bookingQueue() {
+        return new Queue(BOOKING_QUEUE, true);
+    }
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(NOTIFICATION_QUEUE, true);
+    }
+
+    // --- Exchange (роутер сообщений) ---
+    @Bean
+    public TopicExchange paymentExchange() {
+        return new TopicExchange(PAYMENT_EXCHANGE);
+    }
+
+    @Bean
+    public TopicExchange notificationExchange() {
+        return new TopicExchange(NOTIFICATION_EXCHANGE);
+    }
+
+    @Bean
+    public TopicExchange bookingExchange() {
+        return new TopicExchange(BOOKING_EXCHANGE);
+    }
+
+    // --- Binding (связываем очередь с exchange через routing key) ---
+    @Bean
+    public Binding paymentBinding(Queue paymentQueue, TopicExchange paymentExchange) {
+        return BindingBuilder
+                .bind(paymentQueue)
+                .to(paymentExchange)
+                .with(PAYMENT_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding notificationBinding(Queue notificationQueue, TopicExchange notificationExchange) {
+        return BindingBuilder
+                .bind(notificationQueue)
+                .to(notificationExchange)
+                .with(NOTIFICATION_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding bookingBinding(Queue bookingQueue, TopicExchange bookingExchange) {
+        return BindingBuilder
+                .bind(bookingQueue)
+                .to(bookingExchange)
+                .with(BOOKING_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+}
+
